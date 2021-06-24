4c5958a77e5e3854337b86048171ce14ab3eb89d
migf1 <mig_f1@hotmail.com>
2021-06-24 23:22:02 +0300

    zlib license added

--- msutimer.h
+++ msutimer.h
@@ -1,3 +1,25 @@
+/*
+zlib License
+--------------------------------------------------
+Copyright (c) 2021 migf1@hotmail.com
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+--------------------------------------------------
+*/
 /**
  * @file	msutimer.h
  * @version	0.01 alpha
@@ -7,6 +29,8 @@
  *		ISO C99
  * @par Online Docs:
  *		https://migf1.github.io/msutimer-docs/
+ * @par License
+ *		This project is released under the zlib license (see LICENSE.txt)
  *
  * @brief Simple High Resolution Timer for C (public interface).
  */
